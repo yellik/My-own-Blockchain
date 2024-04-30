35223a627b9fe914aeb36ea829bf91cca098fb82
Cryptokitana <yelenakensborn@gmail.com>
2024-04-30 13:21:54 +0200

    added block class

--- models/Block.mjs
+++ models/Block.mjs
@@ -1,11 +1,12 @@
 
 
 export default class Block {
-    constructor(blockIndex, prevBlockHash, nextBlockHash){
+    constructor(blockIndex, prevBlockHash, nextBlockHash, data){
+        this.timestamp = Date.now ()
         this.blockIndex = blockIndex;
         this.prevBlockHash = prevBlockHash;
         this.nextBlockHash = nextBlockHash;
-        this.data = data
+        this.data = data;
     }
 }
 
